004b5bf3ee4eb9e5eb5e951f3934fb48b18efb5b
brownie79 <icarus@localhost.localdomain>
2016-08-27 18:56:09 -0500

    added mongo drivers

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 //import statements
+    //app tie ins
     const express = require('express');
     const app = express();
+    const url = require('url');
 
+    //scripts
+    const htmlhandler = require('')
 //port listener
 app.listen(80, (err) => {
     if(err){console.log("Cannot listen on port 80: ", err);}
@@ -9,31 +13,13 @@ app.listen(80, (err) => {
 });
 
 
-app.get("/building1", (request, response) => {
-    //show HTML page owners
+app.get("/building", (request, response) => {
+    let querydata = url.parse(request.url, true); //the true makes it into an object
+    htmlhandler.building(querydata.id, response);
 });
 
-app.get("/building2", (request, response) => {
-    //show HTML page owners
-});
-
-// buildings //hard coded to avoid login
-app.get("/building3", (request, response) => {
-    //show HTML page owners
-});
-
-app.get("/building4", (request, response) => {
-    //show HTML page owners
-});
-
-app.get("/building5", (request, response) => {
-    //show HTML page owners
-});
-
-app.get("/renter1", (request, response) => {
-
-});
-
-app.get("/renter1", (request, response) => {
-    
+app.get("/renter", (request, response) => {
+    let querydata = url.parse(request.url);
+    htmlhandler.building = url.parse(request.url, true);
+    htmlhandler.renter(querydata.id, response);
 });
---
+++ js/htmlhandler.js
@@ -0,0 +1,11 @@
+//imports
+    const fs = require('fs');
+
+module.exports = {
+    building : function(id, response){
+        //fetch building data based on database
+    },
+    renter : function(id, response){
+        //fetch building data based on database
+    }
+}
