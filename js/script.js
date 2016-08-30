5cf4f322d2d39a3b3987f89e895c7918bc7565a9
Patrick Bartman <pbartman@hawk.iit.edu>
2016-08-30 17:09:52 -0500

    modified:   css/style.css 	modified:   database_sample/buildings.json 	new file:   html/building.html 	modified:   html/index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,48 @@ jQuery(function() {
 	jQuery('.card').height(h - 248);
 	
 	
-	jQuery('.selected button').attr("disabled", "disabled");
+	//jQuery('.selected button:first-of-type').attr("disabled", "disabled");
 	
 	
-	jQuery('button').click(function() {
-		jQuery('.selected button').removeAttr("disabled").parent().parent().removeClass('selected');
-		jQuery(this).attr("disabled", "disabled").parent().parent().addClass('selected');
+	jQuery('button:first-of-type').click(function() {			//FIXME
+		//jQuery('button').removeAttr("disabled");//.parent().parent().removeClass('selected');
+		jQuery(this)/*.attr("disabled", "disabled").next().removeAttr("disabled")*/.parent().parent().addClass('selected');
+	});	
+	jQuery('button:nth-of-type(2)').click(function() {
+		//jQuery('.selected button')/*.removeAttr("disabled")*/.parent().parent().removeClass('selected');
+		jQuery(this)/*.attr("disabled", "disabled")*/.parent().parent().removeClass('selected');
 	});
 });
+
+
+
+var deadline = 'August 31 2016 23:59:59 GMT+1800';
+
+function getTimeRemaining(endtime){
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor( (t/1000) % 60 );
+	var minutes = Math.floor( (t/1000/60) % 60 );
+	var hours = Math.floor( (t/(1000*60*60)) % 24 );
+	var days = Math.floor( t/(1000*60*60*24) );
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours.toLocaleString('en-IN', { minimumIntegerDigits: 2 }),
+		'minutes': minutes.toLocaleString('en-IN', { minimumIntegerDigits: 2 }),
+		'seconds': seconds.toLocaleString('en-IN', { minimumIntegerDigits: 2 })
+	};
+}
+
+function initializeClock(id, endtime){
+	var clock = document.getElementById(id);
+	var timeinterval = setInterval(function(){
+		var t = getTimeRemaining(endtime);
+		clock.innerHTML = 	t.days + ':' +
+							t.hours + ':' +
+							t.minutes + ':' +
+							t.seconds;
+		if (t.total<=0){
+			clearInterval(timeinterval);
+	    }
+	}, 1000);
+}
